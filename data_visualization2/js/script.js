3ea8fbda956570385e074e98d4382682996c76bc
amadeatheola <amadeatheola@gmail.com>
2025-10-16 10:54:21 +0800

    Changed the data and charts

--- data_visualization2/js/script.js
+++ data_visualization2/js/script.js
@@ -1,16 +1,12 @@
 // Store chart views globally so updateYear() can access them
 const views = {};
 
-vegaEmbed("#cons", "js/cons_choropleth_map.vg.json", {mode: "vega-lite"})
-  .then(res => views["cons_choropleth_map"] = res.view)
+vegaEmbed("#choropleth_map", "js/choropleth_map.vg.json", {mode: "vega-lite"})
+  .then(res => views["choropleth_map"] = res.view)
   .catch(console.warn);
 
-vegaEmbed("#choropleth_map", "js/prod_choropleth_map.vg.json", {mode: "vega-lite"})
-  .then(res => views["prod_choropleth_map"] = res.view)
-  .catch(console.warn);
-
-vegaEmbed("#asean_choropleth_map", "js/asean_choropleth_map.vg.json", {mode: "vega-lite"})
-  .then(res => views["asean_choropleth_map"] = res.view)
+vegaEmbed("#choropleth_map2", "js/choropleth_map2.vg.json", {mode: "vega-lite"})
+  .then(res => views["choropleth_map2"] = res.view)
   .catch(console.warn);
 
 vegaEmbed("#isotype_dot_plot", "js/isotype_dot_plot.vg.json", {mode: "vega-lite"})
@@ -27,9 +23,5 @@ vegaEmbed("#heatmap", "js/heatmap.vg.json", {mode: "vega-lite"})
 
 vegaEmbed("#line_chart", "js/line_chart.vg.json", {mode: "vega-lite"})
   .then(res => views["line_chart"] = res.view)
-  .catch(console.warn);
-
-vegaEmbed("#choropleth_map2", "js/yrchoropleth_map.vg.json", {mode: "vega-lite"})
-  .then(res => views["choropleth_map2"] = res.view)
   .catch(console.warn);
 
